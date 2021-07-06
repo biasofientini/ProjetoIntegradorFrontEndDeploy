859f0cd5901e16c6be02f0f044ce8657f7e2d0d1
ThamiresHonorato <thamireshonorato1998@gmail.com>
2021-07-06 11:31:04 -0300

    Componente lista produtos-admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HomeComponent } from './home/home.component';
 import { AllProductsComponent } from './all-products/all-products.component';
 import { ProductComponent } from './product/product.component';
 import { ImgComponent } from './img/img.component';
+import { ProductListAdmComponent } from './product-list-adm/product-list-adm.component';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { ImgComponent } from './img/img.component';
     HomeComponent,
     AllProductsComponent,
     ProductComponent,
-    ImgComponent
+    ImgComponent,
+    ProductListAdmComponent
   ],
   imports: [
     BrowserModule,
