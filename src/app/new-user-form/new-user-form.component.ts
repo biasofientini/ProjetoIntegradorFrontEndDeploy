6dbb94ce4a4a044d4ef3a25a0a1dbe35ef5a5af7
biasofientini <biasofientini@gmail.com>
2021-07-13 22:23:05 -0300

    Mudancas no About e Stack

--- src/app/new-user-form/new-user-form.component.ts
+++ src/app/new-user-form/new-user-form.component.ts
@@ -77,22 +77,6 @@ export class NewUserFormComponent implements OnInit {
   admin(event: any) {
     this.roleId = Number(event.target.value)
   }
-  
-                                                                
-  confirmarSenha(event: any) {
-    this.confirmeSenha = event.target.value
-  }
-
-  adminCheckbox(event: any) {
-    if (this.checkboxAdmin.nativeElement.checked == true) {
-      this.adminCheck = true
-      this.roleId = 1
-    }
-    if (this.checkboxAdmin.nativeElement.checked == false) {
-      this.adminCheck = false
-      this.roleId = 2
-    }
-  }
 
   newUser() {
     if(!this.validateInput()) return
